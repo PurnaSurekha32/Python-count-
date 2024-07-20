8d407847aa3d8fa9a0392c6c9083ae5d83408b9d
PurnaSurekha32 <purna32surekha@gmail.com>
2024-07-20 10:48:05 +0530

    Update Count alpha,num,space..py

--- Count alpha,num,space..py
+++ Count alpha,num,space..py
@@ -1,14 +1,16 @@
 
 
-
-'''s="Python is easy to learn and int ex 123456"
+# First Method
+s="Python is easy to learn and int ex 123456"
 print("The no.Spaces are:",s.count(" "))
 alpha= sum(1 for char in s if char.isalpha())
 print("The no. of alphabets are:", alpha)
 digits = sum(1 for char in s if char.isdigit())
-print("The no. of numbers are:", digits)'''
+print("The no. of numbers are:", digits)
 
-'''S="Python is easy to learn and int ex 123456"
+
+#Second Method
+S="Python is easy to learn and int ex 123456"
 w=0
 s=0
 n=0
@@ -33,24 +35,9 @@ print("Space",s)
 print("Number",n)
 print("Lower case:",l)
 print("Upper case:",u)
-print("ascii:",c)'''
+print("ascii:",c)
 
-'''a=1,2,3,4,5,6,7,8,9
-even= sum(1 for num in a if num%2==0)
-print("The no. of even are:", even)
-odd = sum(1 for num in a if num%2!=0)
-print("The no. of odd are:", odd)'''
 
-a=1,2,3,4,5,6
-even=0
-odd=0
-for i in range (a):
-    if i%2==0:
-        even=even+1
-    else:
-        odd=odd+1
-print(even)
-print(odd)
             
     
 
